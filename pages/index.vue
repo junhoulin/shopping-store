b257f21caa01710c62069f0d500871b4cdb5188c
jim-home <yy6313531@gmail.com>
2024-12-30 22:03:55 +0800

    reset all.scss

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        11111111111111
+        <span>dssdsdsddsd</span>
+        <button>asd</button>
+        <span>dssdsdsddsd</span>
+
+        <span>dssdsdsddsd</span>
+        <span>dssdsdsddsd</span>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/product/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        sadasdasdasd
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
